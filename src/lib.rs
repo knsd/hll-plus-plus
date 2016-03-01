1238d6cc3c75cdfdc68599c02994eeabbe9c2ac9
Fedor Gogolev <knsd@knsd.net>
2016-03-01 14:28:48 +0300

    Implement initial strucutres and constructors

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,49 @@
 
-pub enum Mode {
-    Sparse,
-    Dense,
+use std::collections::{HashSet};
+
+
+pub struct CompressedList {
+    count: u32,
+    last: u32,
+    bytes: Vec<u8>,
 }
 
-pub struct Hll {
-    m: u32,
-    p: u8,
-    registers: Vec<u8>,
+impl CompressedList {
+    fn new(size: usize) -> CompressedList {
+        CompressedList {
+            count: 0,
+            last: 0,
+            bytes: vec![0; size]
+        }
+    }
+}
+
+pub enum Hll {
+    Sparse {
+        m: u32,
+        p: u8,
+        set: HashSet<u32>,
+        list: CompressedList,
+    },
+    Dense {
+        m: u32,
+        p: u8,
+        registers: Vec<u8>
+    },
+}
+
+impl Hll {
+    fn new(precision: u8) -> Option<Hll> {
+        if precision > 18 || precision < 4 {
+            return None
+        }
+
+        let m = 1 << precision as u32;
+        Some(Hll::Sparse {
+            m: m,
+            p: precision,
+            set: HashSet::new(),
+            list: CompressedList::new(m as usize)
+        })
+    }
 }
